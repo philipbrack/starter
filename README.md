b2f49f87397418ed6c53ed934577a5cb2fcaac44
J.P. Poveda <jp@poveda.net>
2016-11-28 22:37:21 -0800

    Sample updated to account for 0.1.0 of SakuraApi

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # starter
-A starter project for SakuraAPI
+
+A sample project demonstrating the use of SakuraApi.
+
+You should be able to:
+
+```
+npm install
+npm start
+```
+
+Which will start the server. You can then browse to: http://localhost:8001/api/greeting
+
+You should see:
+
+```
+{"greeting":"Hello World"}
+```
---
+++ index.ts
@@ -0,0 +1,35 @@
+import {
+  Routable,
+  Route,
+  SakuraApi
+}                   from 'sakuraapi';
+import * as colors  from 'colors';
+import * as express from 'express';
+
+@Routable({
+  baseUrl: 'api'
+})
+class Hello {
+
+  @Route({
+    path: 'greeting',
+    method: 'get'
+  })
+  getGreeting(req: express.Request, res: express.Response) {
+    res.status(200).json({
+      greeting: 'Hello World'
+    });
+  }
+}
+
+(function boot() {
+  let sapi = SakuraApi.instance;
+
+  sapi.route(new Hello());
+
+  sapi
+    .listen()
+    .catch((err) => {
+      console.log(`Error: ${err}`.red);
+    });
+})();
